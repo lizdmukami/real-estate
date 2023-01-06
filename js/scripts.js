8d51b6e4a7466056ea28bf05c3abd72f6a53ff01
lizdmukami <lizdmukami@gmail.com>
2023-01-06 05:47:42 +0300

    Added more html pages and changed the others to accomodate these new pages

--- js/scripts.js
+++ js/scripts.js
@@ -37,53 +37,51 @@ function toggleMenu() {
     navbar.classList.remove("fixed");
   }
 }
+// CAROUSEL SLIDER
 
-	// CAROUSEL SLIDER
+const myslide = document.querySelectorAll('.my-slide'),
+dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
 
-	const myslide = document.querySelectorAll('.my-slide'),
-	dot = document.querySelectorAll('.dot');
-	let counter = 1;
-	slidefun(counter);
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+counter += 1;
+slidefun(counter);
+}
+function plusSlides(n) {
+counter += n;
+slidefun(counter);
+resetTimer();
+}
+function currentSlide(n) {
+counter = n;
+slidefun(counter);
+resetTimer();
+}
+function resetTimer() {
+clearInterval(timer);
+timer = setInterval(autoSlide, 8000);
+}
 
-	let timer = setInterval(autoSlide, 8000);
-	function autoSlide() {
-	counter += 1;
-	slidefun(counter);
-	}
-	function plusSlides(n) {
-	counter += n;
-	slidefun(counter);
-	resetTimer();
-	}
-	function currentSlide(n) {
-	counter = n;
-	slidefun(counter);
-	resetTimer();
-	}
-	function resetTimer() {
-	clearInterval(timer);
-	timer = setInterval(autoSlide, 8000);
-	}
+function slidefun(n) {
 
-	function slidefun(n) {
-
-	let i;
-	for(i = 0;i<myslide.length;i++){
-	myslide[i].style.display = "none";
-	}
-	for(i = 0;i<dot.length;i++) {
-	dot[i].className = dot[i].className.replace(' active', '');
-	}
-	if(n > myslide.length){
-	counter = 1;
-	}
-	if(n < 1){
-	counter = myslide.length;
-	}
-	myslide[counter - 1].style.display = "block";
-	dot[counter - 1].className += " active";
-	}
-	//end of projects section
-   
+let i;
+for(i = 0;i<myslide.length;i++){
+myslide[i].style.display = "none";
+}
+for(i = 0;i<dot.length;i++) {
+dot[i].className = dot[i].className.replace(' active', '');
+}
+if(n > myslide.length){
+counter = 1;
+}
+if(n < 1){
+counter = myslide.length;
+}
+myslide[counter - 1].style.display = "block";
+dot[counter - 1].className += " active";
+}
+//end of projects section
 
 
